093e8cf887e886bfdf52192664e8f76ab68c930f
Biswajit6997 <biswajitde1996@gmail.com>
2024-01-15 12:32:14 +0530

    add vedio 2

---
+++ Variable&datatype&Object/script.js
@@ -0,0 +1,34 @@
+console.log("Hello from object and variable ");
+
+var a=5;
+var b=4;
+console.log(b);
+c="biswjit"
+console.log(a+b+"Biswajit");
+console.log(typeof a,typeof b,typeof c);
+
+var b=45;
+console.log(b);
+
+// datatype
+
+let x="harry nhai";
+let y=22;
+let z=3.55;
+let p=true;
+let q=undefined;
+let r=null;
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+
+// objects
+
+let o={
+    name:"biswajit",
+    salary:"2000000",
+    "job role":"Senior software developer"
+
+};
+console.log(o);
+o.salary="3434crore";
+console.log(o);
--- introduction/script.js
+++ introduction/script.js
@@ -1,1 +1,14 @@
 console.log("hello from codewith harry")
+// promt only use in windows
+var a =prompt("Enter yoour name");
+console.log("my name is "+a)
+// var
+var isTrue=confirm("are you sure to blust your computer");
+
+
+if(isTrue){
+    console.log("your computer is blusting")
+}
+else{
+    console.log("your computer is not blusting")
+}
