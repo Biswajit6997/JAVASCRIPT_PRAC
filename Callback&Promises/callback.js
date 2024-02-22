b480a4f052f38ada03a8704ab77a256f7884a883
Biswajit6997 <biswajitde1996@gmail.com>
2024-02-22 09:59:26 +0530

    add callback function

---
+++ Callback&Promises/callback.js
@@ -0,0 +1,13 @@
+console.log("dfsagdsag");
+const callback=(arg)=>{
+    console.log(arg);
+}
+
+const loadscript=(src,callback)=>{
+    let sc=document.createElement("script");
+    sc.src=src;
+    sc.onload=callback("Biswajit");
+    document.head.append(sc);
+    
+}
+loadscript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js",callback)
--- Callback&Promises/script.js
+++ Callback&Promises/script.js
@@ -3,5 +3,8 @@ console.log("hello world");
 setTimeout(()=>{
     console.log("biswajit");
 },2000)
+setTimeout(()=>{
+console.log("I am here in settimeout 2");
+},2000)
 console.log("end");
  
